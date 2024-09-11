091e6597d4d9f01a87188e3d269548720013e565
ylxmf <ethanelift@gmail.com>
2024-09-11 21:57:27 +0800

    Update: Improve pipeline

--- README.md
+++ README.md
@@ -12,6 +12,16 @@ Install requirements.txt, set the DEEPSEEK_API_KEY in .env file, and then run `p
 
 ### Example 
 - 5daydUwrsSo.srt (with marker and human comparison)
-- rUTAh4gFGaQ.srt (with multiple sessions and human comparison)
-- 7qVNnXiSwks.srt (with multiple sessions, marker and human comparison)
+  - https://www.diffchecker.com/sW56GoYl/
+- rUTAh4gFGaQ.srt (with 3 sessions and human comparison)
+  - https://www.diffchecker.com/Okue5Zar/
+  - https://www.diffchecker.com/VldlBvBD/
+  - https://www.diffchecker.com/D3rCLRow/
+- 7qVNnXiSwks.srt (with 2 sessions, marker, marker(coach), marker(coachee) and human cohttps://www.diffchecker.com/FyIAHJh3/mparison)
+  - https://www.diffchecker.com/7ER71jSr/
+  - https://www.diffchecker.com/o7zTIRrn/
+  - Cannot label marker(coach) or marker(coachee)
+  - If there is a marker at end, the session range may not be able to include the marker.
+
+
 
--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from dotenv import load_dotenv
 import time
 import re
 import json
+import logging
 
 class TranscriptProcessor:
     def __init__(self, input_folder, output_folder, log_folder, api_base_url, api_key, model_name, max_tokens, retry_limit=3):
@@ -52,39 +53,79 @@ class TranscriptProcessor:
         self.raw_transcription = ""
         cnt = 0
         with open(input_path, "r") as f:
-            text = f.readlines()
-            for index in range(2, len(text), 4):
-                item = text[index]
-                cnt += 1
+            self.text = f.readlines()
+            
+            for index in range(2, len(self.text), 4):
+                item = self.text[index]
                 self.raw_transcription += f"{cnt}. " + item
+                cnt += 1
                 if not item.endswith("\n"):
                     self.raw_transcription += "\n"
 
     def process_transcript(self, file_name):
+        # print(f"Processing {file_name}...")
+        self.log_path = os.path.join(self.log_folder, file_name.replace(".srt", ".log"))
+        logger = logging.getLogger()
+        logger.setLevel(logging.INFO)
+        
+        if logger.hasHandlers():
+            logger.handlers.clear()
+        
+        file_handler = logging.FileHandler(self.log_path, mode='w')
+        file_handler.setLevel(logging.INFO)
+        file_handler.setFormatter(logging.Formatter('%(asctime)s - %(message)s'))
+        logger.addHandler(file_handler)
+        
+        stream_handler = logging.StreamHandler()
+        stream_handler.setLevel(logging.INFO)
+        stream_handler.setFormatter(logging.Formatter('%(asctime)s - %(message)s'))
+        logger.addHandler(stream_handler)
+        
+        logger.info(f"Processing {file_name}...")
+        
         input_path = os.path.join(self.input_folder, file_name)
         self.read_transcript(input_path)
-        
         response = self.pipe.invoke({"raw_transcription": self.raw_transcription})
-        
         self.save_output(file_name, response)
-                        
-    def replace_speaker(self, text, speaker_map):
+        
+        logger.info(f"Finished processing {file_name}.")
+         
+    def replace_speaker(self, session_content, speaker_map, i):
+        add1 = False
+        if "Session 0" not in speaker_map:
+            add1 = True
+        speaker_map = {key: value[f"Session {i + add1}"] for key, value in speaker_map.items() if "Session" not in key}
+            
         try:
-            for key, value in speaker_map.items():
-                if "Session" in key:
-                    continue
-                if isinstance(value, list):
-                    if (len(value) == 1): value = value[0]
-                    else: value = (value[0] + " and " + value[1])
-                text = text.replace(key, value)
+            for index in range(len(session_content)):
+                for key, value in speaker_map.items():
+                    session_content[index] = session_content[index].replace(key, value)
+                    
+            # Fiter uninvolved and unknown speaker and ask for human help
+            # session_content = [item for item in session_content if "Uninvolved" not in item]
+            session_content_copy = session_content.copy()
+            session_content = []
+            
+            for item in session_content_copy:
+                if "Uninvolved" not in item and "[SPEAKER_" not in item:
+                    session_content.append(item)
+                elif "Uninvolved" in item:
+                    message = f"Uninvolved speaker detected: {item}"
+                    logging.warning(message)
+                else:
+                    message = f"Unknown speaker detected: {item}"
+                    logging.warning(message)
+                    
+            
         except Exception as e:
             print(f"Error replacing speaker: {e}")
-            
-        return text
+        
+        return session_content
     
     def save_output(self, file_name, response):
-        log_path = os.path.join(self.log_folder, file_name.replace(".srt", ".txt"))
-        with open(log_path, "w") as f:
+        response_path = os.path.join(self.log_folder, file_name.replace(".srt", "_response.txt"))
+        
+        with open(response_path, "w") as f:
             f.write(response)
         
         try:
@@ -94,31 +135,73 @@ class TranscriptProcessor:
             print(f"Error extracting JSON: {e}")
             return
         
-        self.raw_transcription = self.replace_speaker(self.raw_transcription, dict)
         transcription_lines = self.raw_transcription.split("\n")
 
         session_keys = [key for key in dict.keys() if key.startswith("Session")]
         
+        # Process each session
         for i, session_key in enumerate(session_keys):
             session_range = dict[session_key]
-            start, end = session_range[0], session_range[1]
-
-            session_content = transcription_lines[start-1:end]
-
-            output_path = os.path.join(self.output_folder, file_name.replace(".srt", f"_{i}.txt"))
+            start, end = session_range[0], session_range[1] + 1
+            session_content = transcription_lines[start:end]
             
+            # Replace speaker
+            session_content = self.replace_speaker(session_content, dict, i)
+            
+            # Extend the session content, need human removal here.
+            next_start = len(transcription_lines)
+            if i < len(session_keys) - 1:
+                next_start = dict[session_keys[i + 1]][0]
+            
+            sub_transcription = transcription_lines[end:next_start]
+            sub_transcription = self.replace_speaker(sub_transcription, dict, i)
+            
+            last_marker_index = None
+            for line in reversed(sub_transcription):
+                if "Marker" in line:
+                    last_marker_index = sub_transcription.index(line)
+                    break
+            if last_marker_index:
+                last_coach_or_coachee_index = None
+                if last_marker_index != len(sub_transcription) - 1:
+                    last_marker_index += 1 # To include coach/coachee after the last marker
+                    
+                for line in reversed(sub_transcription[:last_marker_index + 1]):
+                    if "Coach" in line or "Coachee" in line:
+                        last_coach_or_coachee_index = sub_transcription.index(line)
+                        print(line)
+                        break
+                if last_coach_or_coachee_index:
+                    session_content.extend(sub_transcription[:last_coach_or_coachee_index + 1])
+                # else:
+                #     session_content.extend(sub_transcription[:last_marker_index]) # No need to + 1
+            
+                        
+            # Revert and Save the session content
+            output_path = os.path.join(self.output_folder, file_name.replace(".srt", f"_({i + 1}).txt"))
+                        
             with open(output_path, "w") as f:
-                f.write("\n".join(session_content))
+                for item in session_content:
+                    match = re.match(r'^\d+\.\s', item)
+                    if match:
+                        content_without_number = item[match.end():]
+                    else:
+                        content_without_number = item
+                    
+                    index = int(item.split(". ")[0])
+                    f.write(self.text[4 * index])
+                    f.write(self.text[4 * index + 1])
+                    f.write(content_without_number)
+                    f.write("\n\n")
                 
-            # print(f"Session {i} saved to {output_path}")
 
     def process_all_transcripts(self):
         files = os.listdir(self.input_folder)
         for file_name in files:
+            # print(f"Processing {file_name}...")
             if file_name.endswith(".srt"):
-                flag = self.process_transcript(file_name)
-                if flag:
-                    print(f"Finished processing {file_name}.", end=" ")
+                self.process_transcript(file_name)
+                    
             # time.sleep(1)
 
 if __name__ == "__main__":
@@ -139,7 +222,7 @@ if __name__ == "__main__":
         # api_base_url="https://api.deepseek.com/beta",
         api_key = os.getenv("DEEPSEEK_API_KEY"),
         model_name = "deepseek-chat",
-        max_tokens = 2048,
+        max_tokens = 4096,
         
         # api_base_url = None,
         # model_name = "gpt-4o-2024-08-06",
@@ -147,9 +230,9 @@ if __name__ == "__main__":
         # max_tokens = 2048,
     )
     
-    # processor.process_all_transcripts()
+    processor.process_all_transcripts()
     
     # For single file processing
-    processor.process_transcript("5daydUwrsSo.srt")
+    # processor.process_transcript("5daydUwrsSo.srt")
     # processor.process_transcript("7qVNnXiSwks.srt")
     # processor.process_transcript("rUTAh4gFGaQ.srt")
